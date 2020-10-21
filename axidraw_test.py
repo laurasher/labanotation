08373e9005ed40206b1964760cb498b7acd7eb52
Laura Asher <laura.asher@jhuapl.edu>
2020-10-21 19:40:21 -0400

    Fix issue AGAIN where it saves invisible parts of svg.

---
+++ axidraw_test.py
@@ -0,0 +1,5 @@
+from pyaxidraw import axidraw   
+ad = axidraw.AxiDraw()
+# ad.plot_setup("/Users/asherlk1/Downloads/ad_api_263_r1/test/assets/AxiDraw_trivial.svg")
+ad.plot_setup("/Users/asherlk1/Documents/MICA/Labanotation/svg_output/AxiDraw_trivial.svg")
+ad.plot_run()
